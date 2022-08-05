e674d38bd3061c9c7c90c4cf100f276292ffbeda
Benjamin Ramser <legionaerr@googlemail.com>
2022-08-05 15:02:15 +0200

    chore: 🔧 clean up POST tests

--- client_test.go
+++ client_test.go
@@ -19,6 +19,17 @@ type Payload struct {
 	Data User `json:"data"`
 }
 
+type TCase struct {
+	Method        string
+	Path          string
+	Params        Params
+	Headers       Header
+	Body          string
+	expPath       string
+	expStatusCode int
+	expBody       interface{}
+}
+
 var (
 	ts *httptest.Server
 )
@@ -51,11 +62,12 @@ func setupTestServer() func() {
 					w.WriteHeader(http.StatusOK)
 					fmt.Fprint(w, fixture("response.json"))
 				default:
-					return
+					w.Header().Set("Content-Type", contentTypeJSON)
+					w.WriteHeader(http.StatusNotFound)
 				}
 			case "POST":
 				switch r.URL.Path {
-				case "/test/2":
+				case "/test":
 					w.Header().Set("Content-Type", contentTypeJSON)
 					w.WriteHeader(http.StatusCreated)
 					body, err := ioutil.ReadAll(r.Body)
@@ -69,6 +81,23 @@ func setupTestServer() func() {
 						return
 					}
 					fmt.Fprint(w, res)
+				case "/test/1/2":
+					w.Header().Set("Content-Type", contentTypeJSON)
+					w.WriteHeader(http.StatusCreated)
+					body, err := ioutil.ReadAll(r.Body)
+					if err != nil {
+						http.Error(w, "can't read body", http.StatusBadRequest)
+						return
+					}
+					res, err := strconv.Unquote(string(body))
+					if err != nil {
+						http.Error(w, "can't read body", http.StatusBadRequest)
+						return
+					}
+					fmt.Fprint(w, res)
+				default:
+					w.Header().Set("Content-Type", contentTypeJSON)
+					w.WriteHeader(http.StatusNotFound)
 				}
 			case "PUT":
 				switch r.URL.Path {
@@ -107,17 +136,6 @@ func TestClientGET(t *testing.T) {
 	teardown := setupTestServer()
 	defer teardown()
 
-	type TCase struct {
-		Method        string
-		Path          string
-		Params        Params
-		Headers       Header
-		Body          []byte
-		expPath       string
-		expStatusCode int
-		expBody       interface{}
-	}
-
 	c, err := NewClient(ts.URL, ClientOptions{})
 	if err != nil {
 		t.Error("could not initialize client")
@@ -142,11 +160,11 @@ func TestClientGET(t *testing.T) {
 			defer res.RawResponse.Body.Close()
 
 			if res.Request.URL != ts.URL+tc.expPath {
-				t.Errorf("expected: %v, got: %v", ts.URL+tc.expPath, res.Request.URL)
+				t.Errorf("\n\n expected: %v, got: %v \n\n", ts.URL+tc.expPath, res.Request.URL)
 			}
 
 			if res.StatusCode() != tc.expStatusCode {
-				t.Errorf("expected StatusCode %v, got: %v", tc.expStatusCode, res.StatusCode())
+				t.Errorf("\n\n expected StatusCode %v, got: %v \n\n", tc.expStatusCode, res.StatusCode())
 			}
 
 			if tc.expBody != nil {
@@ -194,6 +212,12 @@ func TestClientGET(t *testing.T) {
 			expBody:       fixture("response.json"),
 			expStatusCode: 200,
 		},
+		"GET fails": {
+			Method:        "GET",
+			Path:          "/test/fails",
+			expPath:       "/test/fails",
+			expStatusCode: 404,
+		},
 	}
 
 	for name, tc := range tests {
@@ -202,49 +226,95 @@ func TestClientGET(t *testing.T) {
 
 }
 
-func TestPostWithBody(t *testing.T) {
+func TestClientPOST(t *testing.T) {
 	teardown := setupTestServer()
 	defer teardown()
 
-	path := "/:path/:id"
-	params := map[string]interface{}{
-		"path": "test",
-		"id":   2,
-	}
-	url := ts.URL + "/test/2"
-	body := fixture("response.json")
-
 	c, err := NewClient(ts.URL, ClientOptions{})
 	if err != nil {
-		t.Error("Cannot initialize client")
+		t.Error("could not initialize client")
 	}
 
-	res, err := c.NR().
-		SetParams(params).
-		SetBody(body).
-		Execute("POST", path)
-	if err != nil {
-		t.Errorf("expected err to be nil got: %v", err)
+	fn := func(tc TCase) func(*testing.T) {
+		return func(t *testing.T) {
+			req := c.NR()
+
+			if tc.Headers != nil {
+				req.SetHeaders(tc.Headers)
+			}
+
+			if tc.Params != nil {
+				req.SetParams(tc.Params)
+			}
+
+			if tc.Body != "" {
+				req.SetBody(tc.Body)
+			}
+
+			res, err := req.Execute(tc.Method, tc.Path)
+			if err != nil {
+				t.Error("failed to request")
+			}
+			defer res.RawResponse.Body.Close()
+
+			if res.Request.URL != ts.URL+tc.expPath {
+				t.Errorf("\n\n expected: %v, got: %v \n\n", ts.URL+tc.expPath, res.Request.URL)
+			}
+
+			if res.StatusCode() != tc.expStatusCode {
+				t.Errorf("\n\n expected StatusCode %v, got: %v \n\n", tc.expStatusCode, res.StatusCode())
+			}
+
+			if tc.expBody != nil {
+				if res.String() != tc.expBody {
+					t.Errorf("failed. Response \n\n %+v \n\n does not match expected response \n\n %+v \n\n", res.String(), tc.expBody)
+					return
+
+				}
+			}
+		}
 	}
 
-	if res.Request.URL != url {
-		t.Errorf("expected url: %v, got: %v", url, res.Request.URL)
+	tests := map[string]TCase{
+		"POST json": {
+			Method: "POST",
+			Path:   "/test",
+			Headers: map[string]string{
+				"Accept":       contentTypeJSON,
+				"Content-Type": contentTypeJSON,
+			},
+			Body:          fixture("response.json"),
+			expPath:       "/test",
+			expBody:       fixture("response.json"),
+			expStatusCode: 201,
+		},
+		"POST json resolve params": {
+			Method: "POST",
+			Path:   "/test/:id1/:id2",
+			Params: Params{
+				"id1": "1",
+				"id2": "2",
+			},
+			Body: fixture("response.json"),
+			Headers: map[string]string{
+				"Accept":       contentTypeJSON,
+				"Content-Type": contentTypeJSON,
+			},
+			expPath:       "/test/1/2",
+			expBody:       fixture("response.json"),
+			expStatusCode: 201,
+		},
+		"POST fails": {
+			Method:        "POST",
+			Path:          "/test/fails",
+			Body:          fixture("response.json"),
+			expPath:       "/test/fails",
+			expStatusCode: 404,
+		},
 	}
 
-	if res.StatusCode() != 201 {
-		t.Errorf("expected status code 201, got: %v", res.StatusCode())
-	}
-
-	if res.Request.RawRequest.Header.Get("Content-Type") != contentTypeJSON {
-		t.Errorf("expected Content-Type: %v, got: %v",
-			contentTypeJSON, res.Request.RawRequest.Header.Get("Content-Type"),
-		)
-	}
-
-	r := &Payload{}
-	err = Unmarshal(res.Header().Get("Content-Type"), res.Body(), r)
-	if err != nil {
-		t.Error("failed to unmarshal response", err)
+	for name, tc := range tests {
+		t.Run(name, fn(tc))
 	}
 }
 
